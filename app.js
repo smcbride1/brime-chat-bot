2951254603820b294648d516bf48330ffab67984
Stephen McBride <stevemcbride3@gmail.com>
2021-03-18 10:41:41 -0400

    Added message logic

--- app.js
+++ app.js
@@ -10,10 +10,18 @@ const ablyCredentials = {
 //Channel's username
 const username;
 
+//Client sessions; manages user command limit
+let clientSessions = {};
+
 let webSocket;
 const connect = () => {
     console.log("Attempting to connect...");
     webSocket = new webSocket(`wss://realtime.ably.io/?key=${ablyCredentials.key}&clientId=${ablyCredentials}&format=json&heartbeats=true&v=1.2&lib=js-web-1.2.6`);
+}
+
+const close = () => {
+    console.log("Closing connection...");
+    websocket.close();
 }
 
 webSocket.onerror = (error) => {
@@ -28,11 +36,6 @@ websocket.onopen = () => {
     console.log("Connected successfully");
 }
 
-const close = () => {
-    console.log("Closing connection...");
-    websocket.close();
-}
-
 webSocket.onmessage = (event) => {
     data = JSON.parse(event.data);
     switch(data.action) {
@@ -43,7 +46,47 @@ webSocket.onmessage = (event) => {
             webSocket.send({"action":14,"channel":`[?rewind=100]${username}`,"msgSerial":0,"presence":[{"action":2,"data":null}]});
             break;
         case 15:
+            recieveMessage(data);
             break;
-        default:
     }
+}
+
+let receiveMessageFunction;
+const onRecieveMessage = (func) => {
+    receiveMessageFunction = func;
+}
+
+const recieveMessage = (data) => {
+    let connectionId = data.messages[0].clientId;
+    if (connectionId in clientSessions) {
+        if ((clientSessions[connectionId].latestTimestamp + 60) < Date.now()) {
+            clearMessages();
+        }
+        if (clientSessions[connectionId].messageCount >= userCommandLimit) {
+            console.log(`User '${connectionId}' has exceeded the user command limit of ${userCommandLimit}`);
+            return
+        }
+    } else {
+        clientSessions[connectionId] = {};
+    }
+    let timestamp = data.timestamp;
+    addMessageToSession(connectionId, timestamp);
+    receiveMessageFunction(data.messages[0].data.message);
+}
+
+const addMessageToSession = (connectionId, timestamp) => {
+    clientSessions[connectionId].timestamp = timestamp;
+    if (connectionId in clientSessions) {
+        clientSessions[connectionId].messageCount = clientSessions[connectionId].messageCount + 1;
+    } else {
+        clientSessions[connectionId].messageCount = 0;
+    }
+}
+
+const clearMessages = (connectionId) => {
+    clientSessions[connectionId] = {};
+}
+
+const currentUnixTimestamp = () => {
+    Math.floor(Date.now() / 1000)
 }
