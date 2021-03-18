dbe844087911b27198078d368fae3d7728291281
Stephen McBride <stevemcbride3@gmail.com>
2021-03-18 07:41:41 -0400

    User command limit const

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //Max number of chat messages that can be sent within a minute
 const chatLimit = 40;
+//Max number of commands per user per minute
+const userCommandLimit = 10;
 //Brime specific Ably credentials
 const ablyCredentials = {
     clientId: process.env.CLIENT_ID,
-    key: process.env.KEY
+    key: process
 };
