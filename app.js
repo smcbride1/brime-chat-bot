e393c3e55afa36f0862978f84c8b092bf225e379
Stephen McBride <stevemcbride3@gmail.com>
2021-03-18 07:38:31 -0400

    Added chat limit and Ably credential consts

---
+++ app.js
@@ -0,0 +1,7 @@
+//Max number of chat messages that can be sent within a minute
+const chatLimit = 40;
+//Brime specific Ably credentials
+const ablyCredentials = {
+    clientId: process.env.CLIENT_ID,
+    key: process.env.KEY
+};
